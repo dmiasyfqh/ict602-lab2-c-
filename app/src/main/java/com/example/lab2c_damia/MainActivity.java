ff9c9e0f2372cae198f4a59e6843a5345a1e31ce
dmiasyfqh <2022645986@student.uitm.edu.my>
2023-11-22 20:00:31 +0800

    my first commit

---
+++ app/src/main/java/com/example/lab2c_damia/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lab2c_damia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent i = new Intent(MainActivity.this, FullImageActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
